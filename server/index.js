53663a4f8ea334f12e44e857615f5284c6a3f207
Amol0501 <amolaggarwal0501@gmail.com>
2023-10-23 17:27:41 +0530

    create database and connect to database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to database");
+}).catch((Error) => {
+    console.log(Error);
+})
 const app = express();
 
 app.listen(3000, () => {
